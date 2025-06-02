5d18363a7420909ab0f573bb389bc3f6c5583329
robertobaciu <robertroby369@gmail.com>
2025-06-03 02:56:11 +0300

    script

--- scripts/run_cppcheck.sh
+++ scripts/run_cppcheck.sh
@@ -3,9 +3,11 @@
 cppcheck --enable=all \
     --inline-suppr \
     --project="${BUILD_DIR:-build}"/compile_commands.json \
-    -i"${BUILD_DIR:-build}" --suppress=":${BUILD_DIR:-build}/" \
-    -i"${EXT_DIR:-ext}" --suppress=":${EXT_DIR:-ext}/" \
-    -i"${GEN_DIR:-generated}" --suppress=":${GEN_DIR:-generated}/" \
-    --suppress=missingIncludeSystem \
+    -i"${BUILD_DIR:-build}" --suppress="*:${BUILD_DIR:-build}/*" \
+    -i"${EXT_DIR:-ext}" --suppress="*:${EXT_DIR:-ext}/*" \
+    -i"${GEN_DIR:-generated}" --suppress="*:${GEN_DIR:-generated}/*" \
+    --suppress=missingIncludeSystem \Add commentMore actions
     --suppress=unmatchedSuppression \
-    --suppress=useStlAlgorithm
+    --suppress=useStlAlgorithm \
+    --check-level=exhaustive \
+    --error-exitcode=1
